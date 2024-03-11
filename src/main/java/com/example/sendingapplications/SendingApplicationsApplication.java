747a94aef284a703775428bdb0f115eeb9f95476
komead <kapitanDG@mail.ru>
2024-03-11 16:15:32 +0300

    создан проект

---
+++ src/main/java/com/example/sendingapplications/SendingApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendingapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sendingapplications/SendingApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendingapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendingApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
